ad9765358ec4ca816f9e8011532cda56d579aea5
NicolasRoulleau <sp.nicor@gmail.com>
2024-03-04 15:43:25 +0100

    daddy signin signup done v.2.0.0

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ require('../models/connection');
 
 
 const User = require('../models/users');
-const {checkBody} = require('../modules/checkbody');
+const {checkbody} = require('../modules/checkbody');
 const uid2 = require('uid2');
 const bcrypt = require('bcrypt');
 
@@ -14,21 +14,21 @@ const bcrypt = require('bcrypt');
 
 router.post('/signup', (req, res) => {
 
-  if (!checkBody(req.body, ['username', 'password'])) {
+  if (!checkbody(req.body, ['userName', 'password'])) {
     res.json({ result: false, error: 'Champ vide ou manquant' });
     return;
   }
 
   //--> Vérification d'un compte déjà existant
 
-  User.findOne({ username: req.body.username }).then(data => {
+  User.findOne({ userName: req.body.userName }).then(data => {
     if (data === null) {
       const hash = bcrypt.hashSync(req.body.password, 10);
 
       const newUser = new User({
         avatar: '',
         userName: req.body.userName,
-        email: '',
+        email: req.body.email,
         password: hash,
         token: uid2(32),
     });
@@ -49,12 +49,12 @@ router.post('/signup', (req, res) => {
 
 router.post('/signin', (req, res) => {
 
-  if (!checkBody(req.body, ['username', 'password'])) {
+  if (!checkbody(req.body, ['userName', 'password'])) {
     res.json({ result: false, error: 'Champ vide ou manquant' });
     return;
   }
 
-  User.findOne({ username: req.body.username }).then(data => {
+  User.findOne({ userName: req.body.userName }).then(data => {
     if (data && bcrypt.compareSync(req.body.password, data.password)) {
       res.json({ result: true, token: data.token });
     } else {
@@ -63,7 +63,13 @@ router.post('/signin', (req, res) => {
   });
 });
 
+//--> Route Put Modifier les paramètres
 
+router.put('/update', (req, res) => {
+  
+ 
+  res.json({ result: true });
+ });
 
 
 module.exports = router;
