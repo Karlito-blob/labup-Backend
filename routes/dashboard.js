bff1cf7bbfb48f911a0aa8abdf2cd72253c094ef
Théo Sirieix <theo.sirieix@gmail.com>
2024-03-12 14:23:21 +0100

    modif router dashboard

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -8,24 +8,40 @@ const Export = require("../models/export");
 
 router.get("/:token", async (req, res) => {
     try {
-        const userData = await User.findone({token: req.params.token})
+        const userData = await User.findOne({ token: req.params.token });
 
         if (!userData) {
             res.json({ result: false, message: "User token not found" });
             return;
         }
 
-        const dataFromModifiedPattern = await ModifiedPattern.find().toArray()
-        const dataFromDocument = await Document.find().toArray()
-        const dataFromExport = await Export.find().toArray()
+        const aggregatedData = await ModifiedPattern.aggregate([
+            {
+                $match: { user: userData._id } // Filtrer les documents de ModifiedPattern pour l'utilisateur spécifié
+            },
+            {
+                $unionWith: {
+                    coll: "documents", // Nom de la deuxième collection
+                    pipeline: [
+                        { $match: { user: userData._id } } // Filtrer les documents de Document pour l'utilisateur spécifié
+                    ]
+                }
+            },
+            {
+                $unionWith: {
+                    coll: "exports", // Nom de la troisième collection
+                    pipeline: [
+                        { $match: { user: userData._id } } // Filtrer les documents de Export pour l'utilisateur spécifié
+                    ]
+                }
+            }
+        ]);
 
-        const aggregatedData = dataFromModifiedPattern.concat(dataFromDocument, dataFromExport);
-        
-        res.json({result: true, dashboardData: aggregatedData})
+        res.json({ result: true, dashboardData: aggregatedData });
     } catch (error) {
-        console.error(err);
+        console.error(error);
         res.status(500).json({ message: "Erreur lors de la récupération des données." });
     }
-})
+});
 
 module.exports = router;
