515f9a6f9f00b6f84220beb8cc39b5ecaf4da30c
Emilien <emiliengir@gmail.com>
2024-09-14 17:10:56 +0200

    ajout CORS identification

--- app.js
+++ app.js
@@ -12,22 +12,29 @@ var usersRouter = require('./routes/users');
 var initialPatternsRouter = require('./routes/initialPatterns');
 var modifiedPatternsRouter = require('./routes/modifiedPatterns');
 var documentsRouter = require("./routes/documents");
-var exportsRouter = require("./routes/exports")
+var exportsRouter = require("./routes/exports");
 var foldersRouter = require("./routes/folders");
 var fontsRouter = require("./routes/fonts");
 var feedRouter = require("./routes/feed");
-var dashboardRouter = require("./routes/dashboard")
+var dashboardRouter = require("./routes/dashboard");
 
 var app = express();
 
+// Configurer CORS pour accepter uniquement l'origine spécifique
 const cors = require('cors');
-app.use(cors());
+const corsOptions = {
+    origin: 'https://labup-backend.vercel.app',
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
+    credentials: true  // Si vous gérez les cookies d'authentification
+};
+app.use(cors(corsOptions));
 
-//middleware pour bodyoarser
+// Middleware pour bodyparser
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//middleware pour gestion des multiples forms data
+// Middleware pour la gestion des fichiers multipart/form-data avec multer
 var multer = require('multer');
 var upload = multer();
 
@@ -47,7 +54,6 @@ app.use("/exports", upload.single('photoFromFront'), exportsRouter);
 app.use("/folders", foldersRouter);
 app.use('/fonts', fontsRouter);
 app.use('/feed', feedRouter);
-
 app.use('/dashboard', dashboardRouter);
 
 module.exports = app;
